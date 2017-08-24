58f2327fe9fe104f755f3edee09ee05c0803af6e
Shane Orend <shane@Shanes-MBP.attlocal.net>
2017-08-23 23:16:14 -0400

    added comments

--- javascript/javas.js
+++ javascript/javas.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
     $(document).on("click", ".person", GifFunk);
 
     function buttons() {
+    	// this puts initial buttons on screen
         for (var i = 0; i < Topic.length; i++) {
             var b = $("<button>");
             b.addClass("person");
@@ -17,6 +18,7 @@ $(document).ready(function() {
     };
 
     function NewButtons() {
+    	//this puts new buttons on screen
         $("#add-buton").on("click", function(event) {
             // console.log("fsdg");
             event.preventDefault()
@@ -29,7 +31,8 @@ $(document).ready(function() {
     };
 
     function GifFunk() {
-    	var ranNum = Math.floor(Math.random() * 20) + 10;
+    	// this applies gifs based on clicking the buttons on screen
+    	var ranNum = Math.floor(Math.random() * 20) + 10;//determines how many gifs to display 
         var gif = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             gif + "&api_key=dc6zaTOxFJmzC&limit="+ ranNum;
@@ -45,13 +48,14 @@ $(document).ready(function() {
                     var gifDiv = $("<div>");
                     var p = $("<p>").text("Rating", results[i].rating);
                     var gifStill = $("<img>");
-                    gifStill.attr("src", results[i].images.fixed_height_still.url); // Do I need .url at end ???????
-                    gifStill.attr("src", results[i].images.fixed_height_still.url); //compare this too lines 65-68 of othet js//
+                    gifStill.attr("src", results[i].images.fixed_height_still.url); 
+                    gifStill.attr("src", results[i].images.fixed_height_still.url); 
                     gifDiv.append(p);
                     gifDiv.append(gifStill);
                     $("#Gifs").prepend(gifDiv);
                 };
                 $("#Gifs").on("click", function() {
+                	// the type attr isnt  being understood 
                     console.log("Hey You clicked I worked Nice Job!!");
                     var state = $(this).attr(results[i].type)
                     console.log(state);
